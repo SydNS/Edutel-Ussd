5c1b7faeb596914c02fde6a97ffdbc996aa680e3
SydNS <40373744+SydNS@users.noreply.github.com>
2021-06-02 22:44:49 -0700

    adding options to selet from

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 // Reads the variables sent via POST
-$sessionId   = $_POST["sessionId"];  
-$serviceCode = $_POST["serviceCode"];  
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
 $text = $_POST["text"];
 //This is the first menu screen
-if ( $text == "" ) {
-$response  = "Hello welcome, Need help with services we deliver \n";
-$response .= "1. Enter 1 to continue";
+if ($text == "") {
+    $response  = "Hello welcome to Edutel, Need help with services we deliver \n";
+    $response .= "1. Enter 1 to continue";
+}
+// Menu for a user who selects '1' from the first menu
+// Will be brought to this second menu screen
+else if ($text == "1") {
+    $response  = "EDUTEL  Pick a Services below \n";
+    $response .= "1. Education Service\n";
+    $response .= "2. Farming Service \n";
+    $response .= "3. Medical Service \n";
+    $response .= "4. Transport Service \n";
 }
